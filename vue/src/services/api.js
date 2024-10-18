fba1ef55f27b59f9dc03e4e7e46fd26986345f3a
youyrenr <A350856951@qq.com>
2024-10-18 14:17:35 +0800

    first

--- vue/src/services/api.js
+++ vue/src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 import { useAuthStore } from '../store/auth'
 
 const api = axios.create({
-    // baseURL: 'http://localhost:8000/'
-    baseURL: 'http://stock.api.bookagent.com.cn/'
+    baseURL: 'http://localhost:8000/'
+    // baseURL: 'http://stock.api.bookagent.com.cn/'
 })
 
 api.interceptors.request.use(
